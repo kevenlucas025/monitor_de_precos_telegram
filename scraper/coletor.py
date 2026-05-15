e37bbff190983c702d7fbbbcdfc7ef9f61ef0b79
Keven Lucas <keven.lucas00@hotmail.com>
2026-05-15 11:43:57 -0300

    novas configs

--- scraper/coletor.py
+++ scraper/coletor.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
 import time
 
@@ -12,17 +14,18 @@ def criar_driver():
 
     options = Options()
 
-    options.binary_location = "/usr/bin/chromium"
-
     options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920,1080")
 
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+    )
 
-    return driver
+    return driver  
 
 
 def gerar_link_afiliado(driver, url_produto):
