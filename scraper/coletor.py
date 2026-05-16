077655d6287782be9a1f5c4acf0f62e3bc6db56e
Keven Lucas <keven.lucas00@hotmail.com>
2026-05-16 17:17:04 -0300

    asdee,wer

--- scraper/coletor.py
+++ scraper/coletor.py
@@ -102,7 +102,11 @@ def coletar_ofertas():
     url = "https://api.mercadolibre.com/sites/MLB/search?q=ofertas"
 
     headers = {
-        "User-Agent": "Mozilla/5.0"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        "Accept": "application/json, text/plain, */*",
+        "Accept-Language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Connection": "keep-alive",
+        "Referer": "https://www.mercadolivre.com.br/",
     }
 
     try:
